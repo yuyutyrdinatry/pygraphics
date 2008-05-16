ad7aa23b7078fecdf47c921005a81a1ba25a8346
ootz0rz <ootz0rz@ccdc15fc-0b2f-0410-8e7f-618692008b0f>
2008-05-16 17:15:26 +0000

    simple distutils script to create and install the PyGraphics library

---
+++ trunk/cpython/__init__.py
@@ -0,0 +1,19 @@
+import os
+
+# IGNORE_FILES is a list of strings, which describe the file names to be 
+# ignored.
+#    i.e. file.name
+IGNORE_FILES = ['os_test.py', '__init__.py', 'pictureclass.py']
+
+__all__ = []
+for root, dirs, files in os.walk(os.getcwd()):
+    for file in IGNORE_FILES:
+        if ( file in files ):
+            files.remove(file)
+            
+    for file in files:
+        if ( file[-2:] == 'py' and root == os.getcwd()):
+            __all__.append(file)
+            
+if __name__ == '__main__':
+    print __all__
---
+++ trunk/setup.py
@@ -0,0 +1,13 @@
+import os
+from distutils.core import setup
+
+#===============================================================================
+# Core Setup Script
+#===============================================================================
+setup(name='PyGraphics',
+      version='1.3.3.7',
+      extra_path = 'PyGraphics',
+      description='Python Graphics Library',
+      packages = [''],
+      package_dir = {'': 'cpython'},
+      )
