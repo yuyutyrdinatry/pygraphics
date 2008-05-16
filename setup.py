2e739db1ca924e678446b4cd27e9f5775cd4b233
ootz0rz <ootz0rz@ccdc15fc-0b2f-0410-8e7f-618692008b0f>
2008-05-16 18:03:48 +0000

    fixed improper package_dir structure

--- setup.py
+++ setup.py
@@ -8,6 +8,6 @@ setup(name='PyGraphics',
       version='1.3.3.7',
       extra_path = 'PyGraphics',
       description='Python Graphics Library',
-      packages = [''],
+      packages = ['', 'UnitTests'],
       package_dir = {'': 'cpython'},
       )
