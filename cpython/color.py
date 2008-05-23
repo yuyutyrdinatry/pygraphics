ed08ad2bbb853e6be0fa29d196c6ac7ec936bc86
C.J.Maddison <C.J.Maddison@ccdc15fc-0b2f-0410-8e7f-618692008b0f>
2008-05-23 15:17:16 +0000

    Added docstrings Reorganized code for readability

--- cpython/color.py
+++ cpython/color.py
@@ -4,72 +4,118 @@ class Color(object):
     '''An RGB color.'''
 
     def __init__(self, r, g, b):
+        '''Create a Color object representing an RGB color 
+        with values (r, g, b).'''
+        
+        # Do we want to normalize this?
         self.r = int(r) % 256
         self.g = int(g) % 256
         self.b = int(b) % 256
 
     def __str__(self):
-        return "color r=" + str(self.get_red()) + " g=" + str(self.get_green()) + \
-            " b=" + str(self.get_blue())
+        '''Return a str representation of this Color.'''
+        
+        return "color r=" + str(self.get_red()) + \
+                    " g=" + str(self.get_green()) + \
+                    " b=" + str(self.get_blue())
 
     def __repr__(self):
-        return "Color(" + str(self.get_red()) + ", " + str(self.get_green()) + \
-            ", " + str(self.get_blue()) + ")"
+        '''Return an executable str representation of this Color.'''
+        
+        return "Color(" + str(self.get_red()) + ", " \
+                        + str(self.get_green()) + ", "\
+                        + str(self.get_blue()) + ")"
 
     def __sub__(self, color):
+        '''Return a Color object with RGB values equal to the difference between
+        this Color and Color color.'''
+        
         return Color(self.r - color.r, self.g - color.g, self.b - color.b)
 
     def __add__(self, color):
+        '''Return a Color object with RGB values equal to the sum of
+        this Color and Color color.'''
+
         return Color(self.r + color.r, self.g + color.g, self.b + color.b)
 
     def __eq__(self, newcolor):
+        '''Return True if this Color has the same RGB values as Color newcolor.'''
+        
         return self.get_red() == newcolor.get_red() and self.get_green() == \
             newcolor.get_green() and self.get_blue() == newcolor.get_blue()
 
     def __ne__(self, newcolor):
+        '''Return True if this Color has different value from Color newcolor.'''
+        
         return not self.__eq__(newcolor)
 
     def distance(self, color):
+        '''Return the Euclidean distance between the RGB values of this Color 
+        and Color color.'''
+        
         r = pow(self.r - color.r, 2)
         g = pow(self.g - color.g, 2)
         b = pow(self.b - color.b, 2)
         return sqrt(r + g + b)
 
     def difference(self, color):
+        '''Return a Color object with RGB values equal to the difference between
+        this Color and Color color.'''
+
         return self - color
 
     def get_rgb(self):
+        '''Return a list of the RGB values of this Color.'''
+        
         return [self.r, self.g, self.b]
 
     def set_rgb(self, atuple):
+        '''Set the RGB values of this Color to those of tuple or list atuple.'''
+        
         self.r = int(atuple[0]) % 256
         self.g = int(atuple[1]) % 256
         self.b = int(atuple[2]) % 256
 
     def get_red(self):
+        '''Return the red value of this Color.'''
+        
         return self.r
 
     def get_green(self):
+        '''Return the green value of this Color.'''
+        
         return self.g
 
     def get_blue(self):
+        '''Return the blue value of this Color.'''
+        
         return self.b
 
     def set_red(self, value):
+        '''Set the red value of this Color to int value.'''
+        
         self.r = int(value) % 256
 
     def set_green(self, value):
+        '''Set the green value of this Color to int value.'''
+        
         self.g = int(value) % 256
 
     def set_blue(self, value):
+        '''Set the blue value of this Color to int value.'''
+        
         self.b = int(value) % 256
 
     def make_lighter(self):
+        '''Make this Color 35% lighter.'''
+        
         self.r = int((255 - self.r) * .35 + self.r)
         self.g = int((255 - self.g) * .35 + self.g)
         self.b = int((255 - self.b) * .35 + self.b)
 
     def make_darker(self):
+        '''Make this Color 35% darker.'''
+        
         self.r = int(self.r * .65)
         self.g = int(self.g * .65)
         self.b = int(self.b * .65)
