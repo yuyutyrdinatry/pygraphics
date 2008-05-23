048df2d25e91ce93012f9636a6009ef06200013e
C.J.Maddison <C.J.Maddison@ccdc15fc-0b2f-0410-8e7f-618692008b0f>
2008-05-23 20:10:36 +0000

    Reorganized code to be more readable. Removed zoomMult instance variable, it was unused.

--- trunk/cpython/openpicturetool.py
+++ trunk/cpython/openpicturetool.py
@@ -1,19 +1,60 @@
 from Tkinter import *
 import Image
 import ImageDraw
-import ImageTk as imtk
+import ImageTk
 import tkFont
 import picture
 
-class OpenPictureTool:
+class OpenPictureTool(object):
+    '''A Picture tool that allows you to find information about digital images.
+       
+    Selecting Pixels:
+    To select a pixel drag (click and hold down) the mouse to the position 
+    you want and then release it to hold that position's information 
+    in the toolbar.
+    
+    X = the x coordinate of the pixel (counting from the left)
+    Y = the y coordinate of the pixel (counting from the top)
+    R = the Red value of the pixel (0 to 255)
+    G = the Green value of the pixel (0 to 255)
+    B = the Blue value of the pixel (0 to 255)
+    
+    Zooming in/out:
+    To Zoom, select the amount of zoom you want from the zoom menu.
+    Less than 100% zooms out and more than 100% zooms in. The 100% zoom level will
+    always return you to your orginal picture.'''
 
-    def __init__(self, file_name):
-        self.file_name = file_name  #'C:/img_1037.jpg'
-        self.picture = picture.make_picture(self.file_name)
+    def __init__(self, filename=None, image=None):
+        '''Create an OpenPictureTool object with Image image or image at file
+        filename.'''
+        
+        if filename:
+            self.filename = filename
+            self.picture = picture.Picture(filename=self.filename)
+        elif image:
+            self.filename = ''
+            self.picture = picture.Picture(image=image)
 
     def run_tool(self, safe):
+        '''Run this OpenPictureTool.'''
+        
         self.root = Tk()
+        self.set_up_zoommenu()
+        self.set_up_frame()
+        self.set_up_canvas()
+        self.set_up_fields()
+        
+        #
+        # start the event loop
+        #
 
+        self.root.mainloop()
+        if(not safe): #used when not running on darwin(Mac) OS
+            sys.exit(0)
+    
+    def set_up_zoommenu(self):
+        '''Set up the zoom menu for this OpenPictureTool.'''
+        
         self.top = Menu(self.root, bd=2)
         self.root.config(menu=self.top)
 
@@ -32,32 +73,31 @@ class OpenPictureTool:
                               underline=0)
         self.zoom.add_command(label='500%', command=lambda : self.zoomf(5.0),
                               underline=0)
-
         self.top.add_cascade(label='Zoom', menu=self.zoom, underline=0)
 
-        #
-        # create a frame and pack it
-        #
+    def set_up_frame(self):
+        '''Set up the Frame for the Picture.'''
+        
+        self.pic_frame = Frame(self.root)
+        self.pic_frame.pack(side=BOTTOM, fill=X)
+        
+        self.root.image = self.picture.get_image()
+        self.root.orig_image = self.root.image
+        self.root.photoimage = ImageTk.PhotoImage(image=self.root.image)
+        
+        self.root.title(self.filename)
 
-        self.frame1 = Frame(self.root)
-
-        self.frame1.pack(side=BOTTOM, fill=X)
-
-        self.root.im = Image.open(self.file_name).convert("RGB")
-        self.root.original = self.root.im
-        self.root.zoomMult = 1.0
-
-        self.root.photo1 = imtk.PhotoImage(image=self.root.im)
-
-        self.root.title(self.file_name)
-
-        #Canvas for the Image, with scroll bars
-
-        self.canvas1 = Canvas(self.frame1, width=self.root.photo1.width() -
-                              1, height=self.root.photo1.height() - 1,
+    def set_up_canvas(self):
+        '''Set up the Canvas in this OpenPictureTool.'''
+        
+        self.canvas1 = Canvas(self.pic_frame, 
+                              width=self.root.photoimage.width() - 1,
+                              height=self.root.photoimage.height() - 1,
                               cursor="crosshair", borderwidth=0)
-        self.root.vbar = Scrollbar(self.frame1)
-        self.root.hbar = Scrollbar(self.frame1, orient='horizontal')
+        
+        # Scrollbars
+        self.root.vbar = Scrollbar(self.pic_frame)
+        self.root.hbar = Scrollbar(self.pic_frame, orient='horizontal')
         self.root.vbar.pack(side=RIGHT, fill=Y)
         self.root.hbar.pack(side=BOTTOM, fill=X)
 
@@ -67,12 +107,14 @@ class OpenPictureTool:
         self.root.hbar.config(command=self.canvas1.xview)
         self.canvas1.config(yscrollcommand=self.root.vbar.set)  # call on canvas move
         self.canvas1.config(xscrollcommand=self.root.hbar.set)
-        self.drawImage(self.root.im)
+        
+        self.draw_image(self.root.image)
 
-        self.canvas1.bind('<Button-1>', self.canvClick)
-
+        self.canvas1.bind('<Button-1>', self.canvas_click)
+    
+    def set_up_fields(self):
+        
         #Entry fields and Enter button
-
         fields = ('X:', 'Y:')
 
         self.root.bind('<Return>', lambda event: self.fetch(self.ents))  #enter key available
@@ -104,40 +146,26 @@ class OpenPictureTool:
                          self.fetch(self.ents)).pack(side=TOP, padx=6,
                 pady=1, anchor=CENTER, fill=BOTH)
 
-        #
-        # start the event loop
-        #
 
-        self.root.mainloop()
-        if(not safe): #used when not running on darwin(Mac) OS
-            sys.exit(0)
 
     def zoomf(self, factor):
-
-        # zoom in or out in increments
-
-        imgpil = self.root.original
-        (wide, high) = imgpil.size
-
-        #root.zoomMult *= factor
-        #if factor < 1.0:                     # antialias best if shrink
-        #    filter = Image.ANTIALIAS         # also nearest, bilinear
-        #else:
-        #    filter = Image.BICUBIC
-
-        new = imgpil.resize((int(wide * factor), int(high * factor)))  #, filter)
-        self.drawImage(new)
+        '''Zoom in or out by a factor of float factor.'''
+        
+        image = self.root.orig_image
+        (wide, high) = image.size
+        new = image.resize((int(wide * factor), int(high * factor)))
+        self.draw_image(new)
 
     def restore(self):
-        self.drawImage(self.root.original)
+        self.draw_image(self.root.orig_image)
 
-    def drawImage(self, imgpil, forcesize=()):
-        self.root.im = imgpil
-        self.root.photo1 = imtk.PhotoImage(image=imgpil)  # not file=imgpath
+    def draw_image(self, image, forcesize=()):
+        self.root.image = image
+        self.root.photoimage = ImageTk.PhotoImage(image=image)  # not file=imgpath
         (scrwide, scrhigh) = forcesize or self.root.maxsize()  # wm screen size x,y
         scrhigh -= 115  # leave room for top display/button at max photo size
-        imgwide = self.root.photo1.width()  # size in pixels
-        imghigh = self.root.photo1.height()  # same as imgpil.size
+        imgwide = self.root.photoimage.width()  # size in pixels
+        imghigh = self.root.photoimage.height()  # same as image.size
 
         fullsize = (0, 0, imgwide, imghigh)  # scrollable
         viewwide = min(imgwide, scrwide)  # viewable
@@ -147,7 +175,7 @@ class OpenPictureTool:
         self.canvas1.config(height=viewhigh, width=viewwide)  # viewable window size
         self.canvas1.config(scrollregion=fullsize)  # scrollable area size
 
-        self.root.img = self.canvas1.create_image(0, 0, image=self.root.photo1,
+        self.root.img = self.canvas1.create_image(0, 0, image=self.root.photoimage,
                 anchor=NW)
 
         if imgwide <= scrwide and imghigh <= scrhigh:  # too big for display?
@@ -158,15 +186,16 @@ class OpenPictureTool:
 
             self.root.state('zoomed')  # others use geometry( )
 
-    def canvClick(self, event):
+    def canvas_click(self, event):
+        
         try:
             x = self.canvas1.canvasx(event.x)
             y = self.canvas1.canvasy(event.y)
-            if x >= 0 and x < self.root.photo1.width() and y >= 0 and y < \
-                self.root.photo1.height():
-                tk_rgb = "#%02x%02x%02x" % self.root.im.getpixel((x, y))
+            if x >= 0 and x < self.root.photoimage.width() and y >= 0 and y < \
+                self.root.photoimage.height():
+                tk_rgb = "#%02x%02x%02x" % self.root.image.getpixel((x, y))
                 self.canvas2.config(bg=tk_rgb)
-                rgb = "R: %d; G: %d; B: %d;" % self.root.im.getpixel((x,
+                rgb = "R: %d; G: %d; B: %d;" % self.root.image.getpixel((x,
                         y))
                 self.v.set(rgb)
                 (evX, evY) = self.ents
@@ -187,11 +216,11 @@ class OpenPictureTool:
         try:
             x = int(strX.get())
             y = int(strY.get())
-            if x >= 0 and x < self.root.photo1.width() and y >= 0 and y < \
-                self.root.photo1.height():
-                tk_rgb = "#%02x%02x%02x" % self.root.im.getpixel((x, y))
+            if x >= 0 and x < self.root.photoimage.width() and y >= 0 and y < \
+                self.root.photoimage.height():
+                tk_rgb = "#%02x%02x%02x" % self.root.image.getpixel((x, y))
                 self.canvas2.config(bg=tk_rgb)
-                rgb = "R: %d; G: %d; B: %d;" % self.root.im.getpixel((x,
+                rgb = "R: %d; G: %d; B: %d;" % self.root.image.getpixel((x,
                         y))
                 self.v.set(rgb)
             else:
@@ -202,4 +231,8 @@ class OpenPictureTool:
             self.v.set(rgb)
             pass
 
+if __name__ == '__main__':
+    p = picture.Picture(100, 100)
+    p.clear(picture.salmon)
+    p.show_in_picture_tool()
 
