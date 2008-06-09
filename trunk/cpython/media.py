3ff392115985a7c4c00aa5cee6d84e60f386d32c
C.J.Maddison <C.J.Maddison@ccdc15fc-0b2f-0410-8e7f-618692008b0f>
2008-06-09 15:53:00 +0000

    media.py is now the top level module where all global convenience functions will be organized.
    Changes:
    - Removed all type checking in favour of error raising in the lower level classes or AttributeError raising.
    - Renamed many functions to more appropriate names:
    load_picture(filename)
    create_picture(w, h, col=white)
    crop(pic)
    darken(col)
    lighten(col)
    choose_file()
    choose_color()
    choose_folder
    
    - Added polymorphous function copy()
    - Change inspect(): Now polymorphous function that calls method .inspect() on the input. Now, any media.py object can be made compatible with the inspect global function
    - Added choose_save_filename()

---
+++ trunk/cpython/media.py
@@ -0,0 +1,300 @@
+from color import *
+from picture import *
+from pixel import *
+import Tkinter as tk
+import tkColorChooser
+import tkFileDialog
+
+##
+## Global picture functions ---------------------------------------------------
+##
+
+
+def load_picture(filename):
+    '''Return a Picture object from filename filename.'''
+
+    return Picture(filename=filename)
+
+
+def create_picture(w, h, col=white):
+    '''Return a Picture w pixels wide and h pixels high. 
+    Default Color col is white.'''
+    
+    return Picture(w, h, col)
+
+
+def crop(pic, x1, y1, x2, y2):
+    '''Crop Picture pic so that only pixels inside the rectangular region 
+    with upper-left coordinates (x1, y1) and lower-right coordinates (x2, y2) 
+    remain.  The new upper-left coordinate is (0, 0).'''
+            
+    pic.crop(x1, y1, x2, y2)
+
+
+def get_pixel(pic, x, y):
+    '''Return the Pixel object at the coordinates (x, y) in Picture pic.'''
+    
+    return pic.get_pixel(x, y)
+
+
+def get_pixels(pic):
+    '''Return a list of Picture pic's Pixels from left to right, 
+    top row to bottom row.'''
+
+    return [pixel for pixel in pic]
+
+
+def get_width(pic):
+    '''Return how many pixels wide Picture pic is.'''
+    
+    return pic.get_width()
+
+
+def get_height(pic):
+    '''Return how many pixels high Picture pic is.'''
+    
+    return pic.get_height()
+
+
+def show(pic):
+    '''Display Picture pic in separate window.'''
+    
+    pic.show()
+
+
+def add_line(pic, x1, y1, x2, y2, col):
+    '''Draw a line of Color col from (x1, y1) to (x2, y2) on Picture pic.'''
+    
+    pic.add_line(col, x1, y1, x2, y2)
+
+
+def add_text(pic, x, y, s, col):
+    '''Draw str s in Color col on Picture pic starting at (x, y).'''
+    
+    pic.add_text(col, x, y, s)
+
+
+def add_rect(pic, x, y, w, h, col):
+    '''Draw an empty rectangle of Color col, width w, and height h 
+    on Picture pic. The upper left corner of the rectangle is at (x, y).'''
+    
+    pic.add_rect(col, x, y, w, h)
+
+
+def add_rect_filled(pic, x, y, w, h, col):
+    '''Draw a filled rectangle of Color col, width w, and height h 
+    on Picture pic. The upper left corner of the rectangle is at (x, y).'''
+    
+    pic.add_rect_filled(col, x, y, w, h)
+
+
+def add_oval(pic, x, y, w, h, col):
+    '''Draw an empty oval of Color col, width w, and height h on Picture pic.
+    The upper left corner of the oval is at (x, y).'''
+    
+    pic.add_oval(col, x, y, w, h)
+
+
+def add_oval_filled(pic, x, y, w, h, col):
+    '''Draw a filled oval of Color col, width w, and height h on Picture pic.
+    The upper left corner of the oval is at (x, y).'''
+    
+    pic.add_oval_filled(col, x, y, w, h)
+
+
+def add_polygon(pic, point_list, col):
+    '''Draw an empty polygon of Color col with corners for every vertex 
+    in list point_list on Picture pic.
+    
+    Note:
+    point_list is a list containing vertices xy coordinates 
+    (ex. [x1,y1,x2,y2,x3,y3]) It should contain at least 
+    three coordinate pairs.'''
+    
+    pic.add_polygon(col, point_list)
+
+
+def add_polygon_filled(pic, point_list, col):
+    '''Draw an empty polygon of Color col with corners for every vertex 
+    in list point_list on Picture pic.
+    
+    Note:
+    point_list is a list containing vertices xy coordinates 
+    (ex. [x1,y1,x2,y2,x3,y3]) It should contain at least 
+    three coordinate pairs.'''
+    
+    pic.add_polygon_filled(col, point_list)
+
+
+def save_as(pic):
+    '''Prompt user to pick a directory and filename then write Picture pic
+    to that filename. Requires that file format is specified in filename 
+    by extensions (e.g. ".jpg", ".gif", ".bmp").'''
+    
+    filename = choose_save_location()
+    pic.save_as(filename)
+
+    
+def save(pic):
+    '''Write Picture pic back to its previous file.'''
+    
+    if pic.get_filename() == '':
+        save_as(pic)
+    else:
+        pic.save()
+
+
+##
+## Global pixel functions ------------------------------------------------------
+##
+
+
+def set_red(pix, r):
+    '''Set the red value of Pixel pix to r.'''
+    
+    pix.set_red(r)
+
+
+def get_red(pix):
+    '''Return the red value of Pixel pix.'''
+    
+    return pix.get_red()
+
+
+def set_blue(pix, b):
+    '''Set the blue value of Pixel pix to b.'''
+
+    pix.set_blue(b)
+
+
+def get_blue(pix):
+    '''Return the blue value of Pixel pix.'''
+    
+    return pix.get_blue()
+
+
+def set_green(pix, g):
+    '''Set the green value of Pixel pix to g.'''
+
+    pix.set_green(g)
+
+
+def get_green(pix):
+    '''Return the green value of Pixel pix.'''
+    
+    return pix.get_green()
+
+
+def get_color(pix):
+    '''Return the Color object with Pixel pix's RGB values.'''
+    
+    return pix.get_color()
+
+
+def set_color(pix, col):
+    '''Set the RGB values of Pixel pix to those of Color col.'''
+
+    pix.set_color(col)
+
+
+def get_x(pix):
+    '''Return the x coordinate of Pixel pix.'''
+    
+    return pix.get_x()
+
+
+def get_y(pix):
+    '''Return the y coordinate of Pixel pix.'''
+    
+    return pix.get_y()
+
+
+##
+## Global color functions ------------------------------------------------------
+##
+
+
+def distance(col1, col2):
+    '''Return the Euclidean distance between the RGB values of Color col1 and
+    Color col2.'''
+    
+    return col1.distance(col2)
+
+
+def darken(col):
+    '''Darken Color col by 35%.'''
+    
+    col.make_darker()
+
+
+def lighten(col):
+    '''Lighten Color col by 35%.'''
+    
+    col.make_lighter()
+
+
+def create_color(r, g, b):
+    '''Return a Color object with RGB values r, g, and b.'''
+    
+    return Color(r, g, b)
+
+##
+## Media functions -------------------------------------------------------
+##
+
+
+def inspect(obj):
+    '''Inspect object obj. Works on most top level media.py objects.'''
+    
+    obj.inspect()
+
+
+def copy(obj):
+    '''Return a deep copy of object obj. Works on most media.py objects.'''
+    
+    return obj.copy()
+
+
+def choose_save_filename():
+    '''Prompt user to pick a directory and filename. Return the path
+    to the new file.'''
+    
+    root = tk.Tk()
+    root.withdraw()
+    path = tkFileDialog.asksaveasfilename()
+    root.destroy()
+    if path:
+        return path
+
+
+def choose_file():
+    '''Prompt user to pick a file. Return the path to that file.'''
+    
+    root = tk.Tk()
+    root.withdraw()
+    path = tkFileDialog.askopenfilename()
+    root.destroy()
+    if path:
+        return path
+
+
+def choose_folder():
+    '''Prompt user to pick a folder. Return the path to that folder.'''
+
+    root = tk.Tk()
+    root.withdraw()
+    folder = tkFileDialog.askdirectory()
+    root.destroy()
+    if folder:
+        return folder
+
+
+def choose_color():
+    '''Prompt user to pick a color. Return a RGB Color object.'''
+    
+    root = Tk()
+    root.withdraw()
+    color = tkColorChooser.askcolor()
+    root.destroy() 
+    if color[0]:
+        return Color(color[0][0], color[0][1], color[0][2])
