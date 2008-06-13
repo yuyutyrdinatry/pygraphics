516b8ae5e7339913cca44ec3d2d51311469e745f
ootz0rz <ootz0rz@ccdc15fc-0b2f-0410-8e7f-618692008b0f>
2008-06-13 15:21:48 +0000

    updated to threading

--- hardeep sandbox/wx/wxShow.py
+++ hardeep sandbox/wx/wxShow.py
@@ -1,7 +1,53 @@
+import threading
 try:
     import wx
 except ImportError:
     raise ImportError,"The wxPython module is required to run this program"
+
+class ShowWrapperThread(threading.Thread):
+    '''Wraps wxShow into it's own thread.'''
+    
+    def __init__(self):
+        threading.Thread.__init__(self, None, self.run, "wxShow")
+        self.setDaemon(1)
+    
+    def set_loop_bind(self, bind):
+        self.bind = bind
+        
+    def run(self):
+        '''Runs when the thread is started. Executes the local function bind.'''
+        self.bind()
+        
+class ShowTimerThread(threading.Thread):
+    '''Timer thread to update the show window.'''
+    stop = False
+        
+    def run(self):
+        '''Runs when the thread is started.'''
+        
+        # Create local variables of all functions, variables, etc... for speed
+        get_time = time
+        poll_interval = self.poll_interval
+        update = self.update_bind
+        
+        a = get_time()
+        while ( not self.stop ):
+            delta_t = get_time() - a 
+            if ( delta_t >= poll_interval ):
+                a = get_time()
+                update()
+                
+    def set_update_bind(self, bind):
+        self.update_bind = bind
+    
+    def set_poll_interval(self, interval=1):
+        '''Set the local poll_interval to the int interval which is the interval
+        in which to update the canvas in seconds.'''
+        self.poll_interval = interval
+    
+    def end(self):
+        '''Set the local boolean self.stop to True.'''
+        self.stop = True
 
 class wxShow(wx.Frame):
     def __init__(self,parent,id,title):
@@ -14,9 +60,8 @@ class wxShow(wx.Frame):
         
 if __name__ == "__main__":
     app = wx.App()
-    frame = wxShow(None,-1,'my application')
+    frame = wxShow(None,-1,'my threaded application')
     
-    import threading
-    t = threading.Thread(None, app.MainLoop, "Show")
-    t.setDaemon(1)
-    t.start() 
+    a = ShowWrapperThread()
+    a.set_loop_bind(app.MainLoop)
+    a.start()
