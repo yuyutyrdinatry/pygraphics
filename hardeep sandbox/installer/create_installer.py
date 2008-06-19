13fa14005bf57c9b1c8aa2b9b90d95196e56d38e
ootz0rz <ootz0rz@ccdc15fc-0b2f-0410-8e7f-618692008b0f>
2008-06-19 08:08:36 +0000

    windows-based dmg creation for mac os distribution

--- hardeep sandbox/installer/create_installer.py
+++ hardeep sandbox/installer/create_installer.py
@@ -2,20 +2,33 @@
 import os
 from subprocess import call
 
-# Path to IzPack build script
+#===============================================================================
+# Paths (configure me)
+#===============================================================================
 izPath = os.path.join('c:\\', 'Program Files', 'IzPack')
 
+# For windows only! Must have valid ImgBurn install
+# ImgBurn: http://www.imgburn.com/
+img_burn = os.path.join('C:\\', 'Program Files', 'ImgBurn', 'ImgBurn.exe')
+
+#===============================================================================
 # Path to the install xml definition
+#===============================================================================
 izInstallXML = "izpack_installer.xml"
 
+#===============================================================================
 # Path to all executables required
+#===============================================================================
 izCompile = os.path.join(izPath, 'bin', 'compile.bat')
 iz2EXE = os.path.join(izPath, 'utils', 'izpack2exe', 'izpack2exe.exe')
 iz2APP = os.path.join(izPath, 'utils', 'izpack2app', 'izpack2app.exe')
 iz7ZA = os.path.join(izPath, 'utils', 'izpack2exe', '7za.exe')
 
+#===============================================================================
 # Create commands
-cmd_compile = '"%s" "%s" %s %s %s' % (izCompile, izInstallXML, '-b .', '-o install.jar', '-k standard')
+#===============================================================================
+cmd_compile = '"%s" "%s" %s %s %s' % (izCompile, izInstallXML, '-b .', 
+                                      '-o install.jar', '-k standard')
 cmd_exe = '"%s" --file="%s" --output="%s" --with-7z="%s"' % \
             (iz2EXE,  
              'install.jar',  
@@ -25,8 +38,20 @@ cmd_app = '"%s" "%s" "%s"' % \
             (iz2APP,  
              'install.jar',  
              'install.app')
+cmd_img = '%s /MODE BUILD /BUILDMODE IMAGEFILE /SRC "%s\\" /DEST "%s" /FILESYSTEM "ISO9660 + UDF" /UDFREVISION "2.50" /VOLUMELABEL "UofT" /CLOSESUCCESS /RECURSESUBDIRECTORIES YES /NOSAVESETTINGS /ROOTFOLDER YES /OVERWRITE YES /START /LOG "%s"' % (
+          img_burn,                                                                                                                                                                                                              
+          os.path.join(os.getcwd(), 'install.app', ''), 
+          os.path.join(os.getcwd(), 'install.dmg'), 
+          os.path.join(os.getcwd(), 'ImgBurn.log'))
+            
+#===============================================================================
+# Create the PyGraphics distutils package
+#===============================================================================
+# call('python setup.py sdist')
 
+#===============================================================================
 # Run commands with output
+#===============================================================================
 print "Compiling installer jar..."
 print cmd_compile
 print "________________________________________________________________________"
@@ -44,5 +69,15 @@ print cmd_app
 print "________________________________________________________________________"
 call(cmd_app)
 print "\n\n\n"
+
+#===============================================================================
+# Create ISO from MacOS .app folder (only windows supported so far)
+#===============================================================================
+if ( os.name == 'nt' ):
+    print "Creating ISO"
+    print cmd_img
+    print "________________________________________________________________________"
+    call(cmd_img)
+    print "\n\n\n"
 
 a = raw_input("Done... press enter to close")
