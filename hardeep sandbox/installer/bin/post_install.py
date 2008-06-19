2de9369df22d4eb812ed4c1ffc78709ff6fb9ae0
ootz0rz <ootz0rz@ccdc15fc-0b2f-0410-8e7f-618692008b0f>
2008-06-19 06:16:49 +0000

    added python-based registry modification to set paths

--- hardeep sandbox/installer/bin/post_install.py
+++ hardeep sandbox/installer/bin/post_install.py
@@ -1,63 +1,75 @@
 # This script executes after all the data has been copied over and Python has
 # been installed.
-#
-# Registry modification code obtained from:
-# http://agiletesting.blogspot.com/2005/06/handling-path-windows-registry-value.html
 
 import os
-from sys import getwindowsversion
 from subprocess import call
 
 #===============================================================================
-# Parse and config
+# Parse (do not modify)
 #===============================================================================
 root_path = "$INSTALL_PATH"
-root_path = "C:\\Program Files\\UofT CSC Dev Pack 2008" #testing only
+root_path = "C:\\Program Files\\UofT CSC Dev Pack 2008" # testing only
+
+#===============================================================================
+# Config (edit as needed)
+#===============================================================================
+wing_file = {}
+wing_file['win'] = 'wingide-101-3.1.1-1.exe'
+wing_file['mac'] = 'wingide-101-3.1.1-1-i386.dmg'
 
 python_path = {}
-python_path['win'] = 'c:\\Python25\\' # default python install location
-python_path['mac'] = '' #probably same as linux...not sure
-python_path['nix'] = '/usr/bin/python'
+python_path['win'] = os.path.join('c:\\', 'Python25') # default python install location
+python_path['mac'] = os.path.join('usr', 'bin') # probably same as linux...not sure
 
-nose_path = os.path.join(root_path, 'nose', 'setup.py')
-PIL_path = os.path.join(root_path, 'PIL', 'setup.py')
-PyG_path = os.path.join(root_path, 'PyGraphics', 'setup.py')
-setx_path = os.path.join(root_path, 'tools', 'setx2k.exe')
+mod_path = {}
+mod_path['nose'] = os.path.join(root_path, 'nose', 'setup.py')
+mod_path['PIL'] = os.path.join(root_path, 'PIL', 'setup.py')
+mod_path['PyG'] = os.path.join(root_path, 'PyGraphics', 'setup.py')
+
+# ---------------------------------------------------------------------------- #
+# You shouldn't need to edit below this line
+# ---------------------------------------------------------------------------- #
+
 #===============================================================================
 # Install Wing
 #===============================================================================
 if ( os.name == 'nt' ): #Windows
-    call([os.path.join(root_path, 'wing', 'win', 'wingide-101-3.1.1-1.exe'), '/SILENT'])
+    call([os.path.join(root_path, 'wing', 'win', wing_file['win']), '/SILENT'])
 elif ( os.name == 'mac' ): #MacOS
-    call([os.path.join(root_path, 'wing', 'mac', 'wingide-101-3.1.1-1-i386.dmg')]) # Figure out if this actually works...
+    call([os.path.join(root_path, 'wing', 'mac', wing_file['mac'])]) # Figure out if this actually works...
 else:
+    # Not going to support linux at this time
     raise Exception("Unsupported OS")
 
 #===============================================================================
-# Set Python Path
+# Set Python Paths (windows)
 #===============================================================================
 if ( os.name == 'nt' ):
+    from sys import getwindowsversion
     major, minor, rev, a, b = getwindowsversion()
-    if ( major == 6 ):
-        call(['setx', 'PATH', '"%PATH%;' + python_path['win']])
-    elif ( major == 5 ):
-        call([setx_path, 'PATH', '"%PATH%;' + python_path['win']])
-    else:
+    
+    if ( major != 6 and major != 5 ): # Not Vista/XP/2000/2003
         raise Exception("Unsupported Windows OS")
+    
+    from win_reg_edit import *
+    WR = WinRegistry()
+    path = WR.get_value("Path")
+    path = path + ';' + python_path['win']
+    WR.set_value("Path", path, None, _winreg.REG_EXPAND_SZ)
 
 #===============================================================================
 # Install nose
 #===============================================================================
-call(['python', nose_path, 'install'])
+call(['python', mod_path['nose'], 'install'])
 
 #===============================================================================
 # Install PIL
 #===============================================================================
-call(['python', PIL_path, 'install'])
+call(['python', mod_path['PIL'], 'install'])
 
 #===============================================================================
 # Install PyGraphics
 #===============================================================================
-call(['python', PyG_path, 'install'])
+call(['python', mod_path['PyG'], 'install'])
 
 a = raw_input('done...')
