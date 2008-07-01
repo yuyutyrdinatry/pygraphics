e853195ef4034ba3b0f68859fd9e420aba30af9e
C.J.Maddison <C.J.Maddison@ccdc15fc-0b2f-0410-8e7f-618692008b0f>
2008-07-01 18:38:55 +0000

    Changed: - can now only pick between stereo and mono - append_silence now in samples as opposed to seconds
    Added:
    
    - __add__(snd), __mul__(int), append(snd), get_channels()
    - support for negative indices to insert, get_sample, crop, and play

--- cpython/sound.py
+++ cpython/sound.py
@@ -2,6 +2,15 @@ import tkSnack
 from sample import *
 import os
 import math
+try:
+    tester = tkSnack.Sound()
+    del tester
+except RuntimeError:
+    import Tkinter as tk
+    root = tk.Tk()
+    root.withdraw()
+    tkSnack.initializeSnack(root)
+
 
 class Sound(object):
     '''A Sound class as a wrapper for the tkSnack object.'''
@@ -12,18 +21,23 @@ class Sound(object):
     # This is the default encoding of 16 bits per sample 
     # allowing a maximum of 32767 and a minimum of -32768 as pressure values
     default_encoding = "Lin16"
-    default_channels = 1
-
-    def __init__(self, seconds=None, samples=None, filename=None, channels=None):
-        '''Create a Sound.
+    channels = 1
+    
+    def __init__(self, stereo=False, filename=None, samples=None, seconds=None):
+        '''Create a Sound. Specify stereo as a boolean value, 
+        otherwise mono as default.
         
         Requires one of:
-        - int seconds, e.g. Sound(10) 
+        - int seconds, e.g. Sound(seconds=10) 
         - named in argument samples, e.g. Sound(samples=2000)
-        - named str argument filename, e.g. Sound(filename='sound.wav').'''
+        - named str argument filename, e.g. Sound(filename='sound.wav').
+        
+        Filename takes precedence over samples, which take precedence 
+        over seconds.'''
         
         self.set_filename(filename)
-        self.channels = channels or self.default_channels
+        if stereo:
+            self.channels = 2
         
         if filename != None:
             snd = tkSnack.Sound(load=filename)
@@ -40,11 +54,16 @@ class Sound(object):
         snd.convert(format=self.default_encoding, 
                     frequency=self.default_samp_rate,
                     channels=self.channels)
+        
+        # convert method appends a few 0 value samples
+        if samples or seconds:
+            snd.crop(0, samples -1)
+            
         self.set_tk_sound(snd)
             
             
     def __str__(self):
-        '''Return a str for this Sound with its length.'''
+        '''Return the number of Samples in this Sound as a str.'''
         
         return "Sound of length " + str(len(self))
 
@@ -58,14 +77,28 @@ class Sound(object):
         elif self.channels == 2:
             for i in range(0, len(self)):
                 yield StereoSample(self.tk_sound, i)
-        else:
-            raise ValueError('Number of channels invalid')
-
+                
 
     def __len__(self):
         '''Return the number of Samples in this Sound.'''
         
         return self.tk_sound.length()
+
+    def __add__(self, snd):
+        '''Return a Sound object with this Sound followed by Sound snd.'''
+        
+        new = self.copy()
+        new.append(snd)
+        return new
+
+
+    def __mul__(self, num):
+        '''Return a Sound object with this Sound repeated num times.'''
+        
+        new = self.copy()
+        for time in range(int(num) - 1):
+            new.append(self)
+        return new
 
 
     def set_tk_sound(self, tk_snd):
@@ -90,32 +123,64 @@ class Sound(object):
 
 
     def append_silence(self, s):
-        '''Append s seconds of silence in all channels to the end 
-        of this Sound.'''
+        '''Append s samples of silence to each of this Sound's channels.
+        If s is negative remove s samples from the end of this Sound.'''
         
-        new_samples = len(self) + (s * self.get_sampling_rate())
-        self.tk_sound.length(new_samples)
+        total_samples = len(self) + s
+        self.tk_sound.length(total_samples)
+        #check if s is negative
+
+
+    def append(self, snd):
+        '''Append Sound snd to this Sound. Requires that snd has same number of
+        channels as this Sound.'''
+        
+        self.tk_sound.concatenate(snd.get_tk_sound())
 
 
     def insert(self, snd, i):
-        '''Insert Sound snd at index i.'''
-        
-        self.tk_sound.insert(snd.get_tk_sound(), i)
+        '''Insert Sound snd at index i. Requires that snd has same number of
+        channels as this Sound.'''
+
+        # Negative indices are supported
+        if -len(self) <= i <= len(self) - 1:            
+            i = i % len(self)
+            self.tk_sound.insert(snd.get_tk_sound(), i)
+        else:
+            raise IndexError('Sample value out of bounds.')
 
 
     def crop(self, first, last):
         '''Crop this Sound so that all Samples before int first and 
-        after int last are removed.'''
+        after int last are removed. Cannot crop to a single sample.'''
+
+        # Negative indices are supported
+        if -len(self) <= first <= len(self) - 1 and \
+            -len(self) <= last <= len(self) - 1: 
+            
+            first = first % len(self)
+            last = last % len(self)
+            self.tk_sound.crop(start=first, end=last)
+        else:
+            raise IndexError('Sample value out of bounds.')
+
+
+    def play(self, first=0, last=-1):
+        '''Play this sound from index first to index last. Play entire
+        Sound as default. If blocking is True do not return until Sound 
+        has finished playing.'''
         
-        self.tk_sound.crop(start=first, end=last)
-
-
-    def play(self, first=0, last=-1, blocking=0):
-        '''Play this sound from sample at first to sample at last. Play entire
-        Sound as default. If blocking is a non-zero number do not return until
-        Sound has finished playing.'''
-
-        self.tk_sound.play(blocking=blocking, start=first, end=last)
+        # Negative indices are supported
+        if -len(self) <= first <= len(self) - 1 and \
+            -len(self) <= last <= len(self) - 1: 
+            
+            first = first % len(self)
+            last = last % len(self)
+            self.tk_sound.play(blocking=False, start=first, end=last)
+        elif len(self) == 0:
+            pass
+        else:
+            raise IndexError('Sample value out of bounds.')
 
 
     def stop(self):
@@ -138,52 +203,57 @@ class Sound(object):
 
 
     def get_sample(self, i):
-        '''Return the Sample object at index i.'''
+        '''Return this Sound's Sample object at index i.'''
 
-        if 0 <= i <= len(self) - 1:
-            if self.channels == 1:
-                return MonoSample(self.tk_sound, i)
-            elif self.channels == 2:
-                return StereoSample(self.tk_sound, i)
-            else:
-                raise ValueError('Number of channels invalid')
-        else:
-            raise IndexError
+        if self.channels == 1:
+            return MonoSample(self.tk_sound, i)
+        elif self.channels == 2:
+            return StereoSample(self.tk_sound, i)
 
     
     def get_max(self):
-        '''Return this Sound's largest sample value.'''
+        '''Return this Sound's highest sample value. If this Sound is stereo
+        return the absolute highest for both channels.'''
         
         largest = 0
-        for sample in self:
-            if self.channels == 1:
+        if self.channels == 1:
+            for sample in self:
                 if largest < sample.get_value():
-                    largest = sample.get_value()
-            if self.channels == 2:
-                if largest < sample.get_left() + sample.get_right():
-                    largest = sample.get_left() + sample.get_right()
+                        largest = sample.get_value()
+        if self.channels == 2:
+            for sample in self:
+                if largest < max(sample.get_left(), sample.get_right()):
+                    largest = max(sample.get_left(), sample.get_right())
         return largest
        
         
     def get_min(self):
-        '''Return this Sound's largest sample value.'''
+        '''Return this Sound's lowest sample value. If this Sound is stereo
+        return the absolute lowest for both channels.'''
         
         smallest = 0
-        for sample in self:
-            if self.channels == 1:
+        if self.channels == 1:
+            for sample in self:
                 if smallest > sample.get_value():
                     smallest = sample.get_value()
-            if self.channels == 2:
-                if smallest > sample.get_left() + sample.get_right():
-                    smallest = sample.get_left() + sample.get_right()
+        if self.channels == 2:
+            for sample in self:
+                if smallest > min(sample.get_left(), sample.get_right()):
+                    smallest = min(sample.get_left(), sample.get_right())
         return smallest
 
 
-    def set_filename(self, filename):
+    def get_channels(self):
+        '''Return the number of channels in this Sound.'''
+        
+        return self.tk_sound['channels']
+    
+
+    def set_filename(self, filename=None):
         '''Set this Sound's filename to filename. If filename is None 
         set this Sound's filename to the empty string.'''
         
-        if filename:
+        if filename != None:
             self.filename = filename
         else:
             self.filename = ''
@@ -208,38 +278,38 @@ class Sound(object):
         self.tk_sound.write(self.filename)
 
 
-def sine_wave(freq, amp, samples):
-    '''Return a synthesized sound of sec seconds in the form of a sine wave 
+def make_sine_wave(freq, amp, samp):
+    '''Return a synthesized sound samp samples long in the form of a sine wave 
     with frequency freq in Hz and amplitude amp in the range [0, 32767].'''
     
-    new_snd = Sound(samples=samples)
+    new_snd = Sound(samples=samp)
     rate = new_snd.get_sampling_rate()
-    interval = 1.0 / freq
-    samples_per_cycle = rate * interval
+    period = 1.0 / freq
+    samples_per_period = rate * period
     max_cycle = 2 * math.pi
     
-    for i in range(len(new_snd)):
+    for i in range(samp):
         curr = new_snd.get_sample(i)
-        raw_val = math.sin((i / samples_per_cycle) * max_cycle)
+        raw_val = math.sin((i / samples_per_period) * max_cycle)
         curr.set_value(int(raw_val * amp))
     
     return new_snd
 
 
-def square_wave(freq, amp, samples):
-    '''Return a synthesized sound of sec seconds in the form of a square wave 
+def make_square_wave(freq, amp, samp):
+    '''Return a synthesized sound samp samples long in the form of a square wave 
     with frequency freq in Hz and amplitude amp in the range [0, 32767].'''
 
-    new_snd = Sound(samples=samples)
+    new_snd = Sound(samples=samp)
     rate = new_snd.get_sampling_rate()
-    interval = 1.0 / freq
-    samples_per_cycle = rate * interval
-    samples_per_halfcycle = int(samples_per_cycle / 2)
+    period = 1.0 / freq
+    samples_per_period = rate * period
+    samples_per_halfperiod = int(samples_per_period / 2)
     sample_val = amp
     
     s = 0
-    for i in range(len(new_snd)):
-        if s > samples_per_halfcycle:
+    for i in range(samp):
+        if s > samples_per_halfperiod:
             sample_val = sample_val * -1
             s = 0
         
@@ -250,81 +320,44 @@ def square_wave(freq, amp, samples):
     return new_snd
 
 
-def add_sounds(snd1, snd2):
-    smaller = min(len(snd1), len(snd2))
-    new_sound = Sound(samples=smaller)
-    
-    for i in range(len(new_sound)):
-        source1 = snd1.get_sample(i)
-        source2 = snd1.get_sample(i)
-        new_samp = new_sound.get_sample(i)
-        new_samp.set_value(source1.get_value() + source2.get_value())
-        
-    return new_sound
-        
-        
-if __name__ == '__main__':
-    import Tkinter as tk
+def make_triangle_wave(freq, amp, samp):
+    '''Return a synthesized sound samp samples long in the form of a square wave 
+    with frequency freq in Hz and amplitude amp in the range [0, 32767].'''
 
-    root = tk.Tk()
-    root.withdraw()
-    tkSnack.initializeSnack(root)
+    new_snd = Sound(samples=samp)
+    rate = new_snd.get_sampling_rate()
+    period = 1.0 / freq
+    samples_per_period = rate * period
+    samples_per_halfperiod = int(samples_per_period / 2)
+    samples_per_quarterperiod = int(samples_per_period / 4)
+    increment = int(amp / samples_per_quarterperiod)
+    sample_val = 0
     
-    s1 = Sound(filename='/work/01 Smack That [feat. Eminem].mp3', channels=2)
-    
-    def normalize(snd):
-        max_samp = snd.get_max()
-        min_samp = snd.get_min()
-        
-        divider = max(abs(max_samp), abs(min_samp))
+    quarter_period = 0
+    half_period = 0
 
-        multiplier = 32767.0 / divider
-        for sample in snd:
-            new_val = sample.get_value() * multiplier
-            sample.set_value(int(new_val))
-    
-    def decrease_vol(snd, factor):
+    for i in range(samp):
         
-        for sample in snd:
-            new_val = sample.get_value() * factor
-            sample.set_value(int(new_val))
-            
-            
-    def echo(snd, delay):
+        if quarter_period > samples_per_quarterperiod:
+            increment = increment * -1
+            quarter_period = 0
+        if half_period > samples_per_halfperiod:
+            increment = increment * -1
+            half_period = 0
         
-        source = snd.copy()
+        curr = new_snd.get_sample(i)
+        sample_val = sample_val + increment
+        curr.set_value(int(sample_val))
         
-        for i in range(delay, len(snd)):
-            source_samp = source.get_sample(i - delay)
-            target_samp = snd.get_sample(i)
+        quarter_period += 1
+        half_period += 1
 
-            echo_val = 0.3 * source_samp.get_value()
-            target_samp.set_value(echo_val + target_samp.get_value())
-        
-        return snd
-    
-    
-    def plot_waveform(snd, width=1024, height=300):
-   
-        win = tk.Toplevel()
-    
-        c = tkSnack.SnackCanvas(win, background="#060", width=width, height=height)
-        c.pack()
-        c.create_waveform(0, 0, fill="#0f0" , sound=snd.tk_sound, width=width, height=height, zerolevel=1)
+    return new_snd
 
-    def plot_spectrogram(snd, width=1024, height=300):
-    
-        win = tk.Toplevel()
-    
-        c = tkSnack.SnackCanvas(win, background="#060", width=width, height=height)
-        c.pack()
-        c.create_spectrogram(0, 0, sound=snd.tk_sound, width=width, height=height)
-    
-    
-    def plot_spectrum(snd, width=1024, height=300):
-        
-        win = tk.Toplevel()
-    
-        c = tkSnack.SnackCanvas(win, background="#060",width=width, height=height)
-        c.pack()
-        c.create_section(0, 0, fill="#0f0", sound=snd.tk_sound, width=width, height=height)
+C = make_sine_wave(264, 6000, 11025)
+D = make_sine_wave(297, 6000, 11025)
+E = make_sine_wave(330, 6000, 11025)
+F = make_sine_wave(352, 6000, 11025)
+G = make_sine_wave(396, 6000, 11025)
+A = make_sine_wave(440, 6000, 11025)
+B = make_sine_wave(494, 6000, 11025)
