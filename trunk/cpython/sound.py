830447ebb0542220a198a1626cd2a3abb59e10a0
C.J.Maddison <C.J.Maddison@ccdc15fc-0b2f-0410-8e7f-618692008b0f>
2008-07-02 19:47:13 +0000

    CONVERSION to PYGAME for implementation:
    Should work as before, with a few changes,
    
    ADDED:
    - 2 helper functions that convert between pygame Sound objects and numpy array objects
    - construction of Sound object from pygame Sound obj
    
    NEW QUIRKS:
    - defaults (encoding, frequency, etc.) can't be changed for each individual sound. They must be set before loading the module and apply universally to all sounds made in that load.
    wave creation functions now check for default channels, removed set_sampling_rate() as a result

--- trunk/cpython/sound.py
+++ trunk/cpython/sound.py
@@ -1,65 +1,135 @@
-import tkSnack
-from sample import *
-import os
-import math
-try:
-    tester = tkSnack.Sound()
-    del tester
-except RuntimeError:
-    import Tkinter as tk
-    root = tk.Tk()
-    root.withdraw()
-    tkSnack.initializeSnack(root)
+'''The Sound class and helper functions. This currently supports only 
+uncompressed .wav files.'''
 
+from sample import *
+import math
+import numpy
+import pygame
+import wave
+
+####################------------------------------------------------------------
+## Defaults
+####################------------------------------------------------------------
+
+# if pygame.mixer is not initialized, do so.
+DEFAULTS = pygame.mixer.get_init()
+if not DEFAULTS:
+    DEFAULT_FREQUENCY = 22050
+    DEFAULT_ENCODING = -16
+    DEFAULT_CHANNELS = 2
+    DEFAULT_BUFFERING = 2048
+    pygame.mixer.pre_init(DEFAULT_FREQUENCY, 
+                          DEFAULT_ENCODING, 
+                          DEFAULT_CHANNELS, 
+                          DEFAULT_BUFFERING)
+    pygame.mixer.init()
+else:
+    DEFAULT_FREQUENCY = DEFAULTS[0]
+    DEFAULT_ENCODING = DEFAULTS[1]
+    
+    # pygame.mixer.get_init() returns channels 0-based, 1 channel is 0, 2 is 1
+    DEFAULT_CHANNELS = DEFAULTS[2] + 1
+
+AUDIO_ENCODINGS = { 8 : numpy.uint8,   # unsigned 8-bit
+     16 : numpy.uint16, # unsigned 16-bit
+     -8 : numpy.int8,   # signed 8-bit
+     -16 : numpy.int16  # signed 16-bit
+     }
+
+####################------------------------------------------------------------
+## Helper functions
+####################------------------------------------------------------------
+
+
+def pygame_to_sample_array(pygame_snd):
+    '''Return a numpy array object, which allows direct access to specific
+    sample values in the buffer of the pygame.mixer.Sound object pygame_snd.'''
+        
+    data = pygame_snd.get_buffer()
+    
+    # Create a numpy array from the buffer with the default encoding
+    array = numpy.frombuffer(data, AUDIO_ENCODINGS[DEFAULT_ENCODING])
+    
+    # If there are two channels make the array object 2D
+    if DEFAULT_CHANNELS == 2:
+        array.shape = (len(array) / 2, 2)
+    return array
+
+
+def sample_array_to_pygame(samp_array):
+    '''Return a new pygame.mixer.Sound object from a numpy array object 
+    samp_array. Requires that samp_array is an appropriate 1D or 2D shape.'''
+
+    shape = samp_array.shape
+    
+    # Check if array has the right shape
+    if DEFAULT_CHANNELS == 1:
+        if len (shape) != 1:
+            raise ValueError, "Array must be 1-dimensional for mono mixer"
+    else:
+        if len (shape) != 2:
+            raise ValueError, "Array must be 2-dimensional for stereo mixer"
+        elif shape[1] != DEFAULT_CHANNELS:
+            raise ValueError, "Array depth must match number of mixer channels"
+    
+    return pygame.mixer.Sound(samp_array)
+
+####################------------------------------------------------------------
+## Sound object
+####################------------------------------------------------------------
 
 class Sound(object):
-    '''A Sound class as a wrapper for the tkSnack object.'''
-    
-    # This is the default frequency of samples per second played
-    default_samp_rate = 22050
-    
-    # This is the default encoding of 16 bits per sample 
-    # allowing a maximum of 32767 and a minimum of -32768 as pressure values
-    default_encoding = "Lin16"
-    channels = 1
-    
-    def __init__(self, stereo=False, filename=None, samples=None, seconds=None):
+    '''A Sound class as a wrapper for the pygame.mixer.Sound object.'''
+        
+    def __init__(self, filename=None, samples=None, seconds=None, sound=None):
         '''Create a Sound. Specify stereo as a boolean value, 
         otherwise mono as default.
         
         Requires one of:
-        - int seconds, e.g. Sound(seconds=10) 
-        - named in argument samples, e.g. Sound(samples=2000)
-        - named str argument filename, e.g. Sound(filename='sound.wav').
+        - named str argument filename (with .wav extension), 
+                e.g. Sound(filename='sound.wav')
+        - named int argument samples, e.g. Sound(samples=2000)
+        - named int argument seconds, e.g. Sound(seconds=10) 
+        - named pygame.mixer.Sound argument sound, 
+                e.g. Sound(sound=pygame.mixer.Sound)
         
         Filename takes precedence over samples, which take precedence 
-        over seconds.'''
+        over seconds, which in turn takes precedence over sound.'''
         
+        self.channels = DEFAULT_CHANNELS
+        self.samp_rate = DEFAULT_FREQUENCY
+        self.numpy_encoding = AUDIO_ENCODINGS[DEFAULT_ENCODING]
+        self.encoding = DEFAULT_ENCODING
         self.set_filename(filename)
-        if stereo:
-            self.channels = 2
         
         if filename != None:
-            snd = tkSnack.Sound(load=filename)
+            snd = pygame.mixer.Sound(filename)
+            
         elif samples != None:
-            snd = tkSnack.Sound()
-            snd.length(samples)
+            if self.channels == 1:
+                
+                # numpy.zeros returns an array object with all 0s in the
+                # specified encoding. In sound terms, this is silence.
+                sample_array = numpy.zeros(samples, self.numpy_encoding)
+            else:
+                sample_array = numpy.zeros((samples, 2), self.numpy_encoding)
+            snd = sample_array_to_pygame(sample_array)
+            
         elif seconds != None:
-            snd = tkSnack.Sound()
-            samples = int(seconds * self.default_samp_rate)
-            snd.length(samples)
+            samples = int(seconds * self.samp_rate)
+            if self.channels == 1:
+                sample_array = numpy.zeros(samples, self.numpy_encoding)
+            else:
+                sample_array = numpy.zeros((samples, 2), self.numpy_encoding)            
+            snd = sample_array_to_pygame(sample_array)
+            
+        elif sound != None:
+            snd = sound
+            
         else:
             raise TypeError("No arguments were given to the Sound constructor.")
-        
-        snd.convert(format=self.default_encoding, 
-                    frequency=self.default_samp_rate,
-                    channels=self.channels)
-        
-        # convert method appends a few 0 value samples
-        if samples or seconds:
-            snd.crop(0, samples -1)
-            
-        self.set_tk_sound(snd)
+                    
+        self.set_pygame_sound(snd)
             
             
     def __str__(self):
@@ -72,17 +142,18 @@ class Sound(object):
         '''Return this Sound's Samples from start to finish.'''
         
         if self.channels == 1:
-            for i in range(0, len(self)):
-                yield MonoSample(self.tk_sound, i)
+            for i in range(len(self)):
+                yield MonoSample(self.samples, i)
         elif self.channels == 2:
-            for i in range(0, len(self)):
-                yield StereoSample(self.tk_sound, i)
+            for i in range(len(self)):
+                yield StereoSample(self.samples, i)
                 
 
     def __len__(self):
         '''Return the number of Samples in this Sound.'''
         
-        return self.tk_sound.length()
+        return len(self.samples)
+
 
     def __add__(self, snd):
         '''Return a Sound object with this Sound followed by Sound snd.'''
@@ -101,152 +172,138 @@ class Sound(object):
         return new
 
 
-    def set_tk_sound(self, tk_snd):
-        '''Set this Sound's tkSnack Sound object.'''
+    def set_pygame_sound(self, pygame_snd):
+        '''Set this Sound's pygame Sound object.'''
         
-        self.tk_sound = tk_snd
+        self.pygame_sound = pygame_snd
+        
+        # self.samples is a numpy array object (either 1D or 2D depending on the
+        # number of channels). This object allows access to specific samples
+        # in the buffer.
+        self.samples = pygame_to_sample_array(pygame_snd)
         
         
-    def get_tk_sound(self):
-        '''Return this Sound's tkSnack Sound object.'''
+    def get_pygame_sound(self):
+        '''Return this Sound's pygame Sound object.'''
         
-        return self.tk_sound
+        return self.pygame_sound
 
 
     def copy(self):
         '''Return a deep copy of this Sound.'''
         
-        samples = len(self)
-        new_snd = Sound(samples=samples)
-        new_snd.tk_sound.copy(self.tk_sound)
-        return new_snd
+        samples = self.samples.copy()
+        new_pygame_snd =  sample_array_to_pygame(samples)
+        return Sound(sound=new_pygame_snd)
 
 
     def append_silence(self, s):
-        '''Append s samples of silence to each of this Sound's channels.
-        If s is negative remove s samples from the end of this Sound.'''
+        '''Append s samples of silence to each of this Sound's channels.'''
         
-        total_samples = len(self) + s
-        self.tk_sound.length(total_samples)
-        #check if s is negative
+        if self.channels == 1:
+            silence_array = numpy.zeros(s, self.numpy_encoding)
+        else:
+            silence_array = numpy.zeros((s, 2), self.numpy_encoding)
+        pygame_silence =  sample_array_to_pygame(silence_array)
+        silence = Sound(sound=pygame_silence)
+        self.append(silence)
 
 
     def append(self, snd):
         '''Append Sound snd to this Sound. Requires that snd has same number of
         channels as this Sound.'''
         
-        self.tk_sound.concatenate(snd.get_tk_sound())
-
-
+        snd_samples = pygame_to_sample_array(snd.get_pygame_sound())
+        my_samples = self.samples
+        new_samples = numpy.concatenate((my_samples, snd_samples))
+        self.set_pygame_sound(sample_array_to_pygame(new_samples))
+    
+    
     def insert(self, snd, i):
         '''Insert Sound snd at index i. Requires that snd has same number of
-        channels as this Sound.'''
+        channels as this Sound. Negative indices are supported.'''
 
-        # Negative indices are supported
-        if -len(self) <= i <= len(self) - 1:            
-            i = i % len(self)
-            self.tk_sound.insert(snd.get_tk_sound(), i)
+        if self.get_channels() == 1 and snd.get_channels() == 1:
+            first_chunk = self.samples[:i]
+            second_chunk = self.samples[i:]
+            new_samples = numpy.concatenate((first_chunk, 
+                                             snd.sound_array, 
+                                             second_chunk))
+            self.set_pygame_sound(sample_array_to_pygame(new_samples))
+        elif self.get_channels() == 2 and snd.get_channels() == 2:
+            first_chunk = self.samples[:i, :]
+            second_chunk = self.samples[i:, :]
+            new_samples = numpy.vstack((first_chunk, 
+                                        snd.sound_array, 
+                                        second_chunk))
+            
+            self.set_pygame_sound(sample_array_to_pygame(new_samples))
         else:
-            raise IndexError('Sample value out of bounds.')
+            raise ValueError("Inserted Sound must have same number of channels.")
 
 
     def crop(self, first, last):
         '''Crop this Sound so that all Samples before int first and 
-        after int last are removed. Cannot crop to a single sample.'''
+        after int last are removed. Cannot crop to a single sample. 
+        Negative indices are supported'''
 
         # Negative indices are supported
-        if -len(self) <= first <= len(self) - 1 and \
-            -len(self) <= last <= len(self) - 1: 
-            
-            first = first % len(self)
-            last = last % len(self)
-            self.tk_sound.crop(start=first, end=last)
-        else:
-            raise IndexError('Sample value out of bounds.')
+        first = first % len(self)
+        last = last % len(self)
+        
+        new_samples = self.samples[first:last + 1]
+        self.set_pygame_sound(sample_array_to_pygame(new_samples))
 
 
     def play(self, first=0, last=-1):
-        '''Play this sound from index first to index last. Play entire
-        Sound as default. If blocking is True do not return until Sound 
-        has finished playing.'''
+        '''Play this Sound from sample index first to last. As default play
+        the entire Sound.'''
         
-        # Negative indices are supported
-        if -len(self) <= first <= len(self) - 1 and \
-            -len(self) <= last <= len(self) - 1: 
-            
-            first = first % len(self)
-            last = last % len(self)
-            self.tk_sound.play(blocking=False, start=first, end=last)
-        elif len(self) == 0:
-            pass
-        else:
-            raise IndexError('Sample value out of bounds.')
+        self.play = self.copy()
+        self.play.crop(first, last)
+        self.play.get_pygame_sound().play()
 
 
     def stop(self):
         '''Stop playing this Sound.'''
         
-        self.tk_sound.stop()
+        self.play.get_pygame_sound().stop()
         
 
     def get_sampling_rate(self):
         '''Return the number of Samples this Sound plays per second.'''
 
-        return self.tk_sound["frequency"]
-
-
-    def set_sampling_rate(self, freq):
-        '''Set the number of Samples this Sound plays per second to
-        the int freq.'''
-        
-        self.tk_sound.configure(frequency=freq)
+        return self.samp_rate
 
 
     def get_sample(self, i):
-        '''Return this Sound's Sample object at index i.'''
+        '''Return this Sound's Sample object at index i. Negative indices are
+        supported. Negative indices are supported.'''
 
         if self.channels == 1:
-            return MonoSample(self.tk_sound, i)
+            return MonoSample(self.samples, i)
         elif self.channels == 2:
-            return StereoSample(self.tk_sound, i)
+            return StereoSample(self.samples, i)
 
     
     def get_max(self):
         '''Return this Sound's highest sample value. If this Sound is stereo
         return the absolute highest for both channels.'''
         
-        largest = 0
-        if self.channels == 1:
-            for sample in self:
-                if largest < sample.get_value():
-                        largest = sample.get_value()
-        if self.channels == 2:
-            for sample in self:
-                if largest < max(sample.get_left(), sample.get_right()):
-                    largest = max(sample.get_left(), sample.get_right())
-        return largest
-       
+        return self.samples.max()
+        
         
     def get_min(self):
         '''Return this Sound's lowest sample value. If this Sound is stereo
         return the absolute lowest for both channels.'''
         
-        smallest = 0
-        if self.channels == 1:
-            for sample in self:
-                if smallest > sample.get_value():
-                    smallest = sample.get_value()
-        if self.channels == 2:
-            for sample in self:
-                if smallest > min(sample.get_left(), sample.get_right()):
-                    smallest = min(sample.get_left(), sample.get_right())
-        return smallest
+        return self.samples.min()
 
 
     def get_channels(self):
         '''Return the number of channels in this Sound.'''
         
-        return self.tk_sound['channels']
+        return self.channels
     
 
     def set_filename(self, filename=None):
@@ -268,18 +325,29 @@ class Sound(object):
     def save_as(self, filename):
         '''Save this Sound to filename filename and set its filename.'''
         
-        self.set_filename(filename)
-        self.tk_sound.write(filename)
-
-    
+        self.set_filename(filename=filename)
+        wav = wave.open(filename, 'w')
+        wav.setnchannels(self.get_channels())
+        
+        # calculate the number of bytes for this sound
+        fmtbytes = (abs(self.encoding) & 0xff) >> 3
+        wav.setsampwidth(fmtbytes)
+        
+        wav.setframerate(self.get_sampling_rate())
+        wav.setnframes(len(self))
+        wav.writeframes(self.pygame_sound.get_buffer().raw)
+        
     def save(self):
         '''Save this Sound to its filename.'''
         
-        self.tk_sound.write(self.filename)
+        self.save_as(self.get_filename())
+        
+####################------------------------------------------------------------
+## Wave creation functions
+####################------------------------------------------------------------
 
-
-def make_sine_wave(freq, amp, samp):
-    '''Return a synthesized sound samp samples long in the form of a sine wave 
+def create_sine_wave(freq, amp, samp):
+    '''Return a Sound samp samples long in the form of a sine wave 
     with frequency freq in Hz and amplitude amp in the range [0, 32767].'''
     
     new_snd = Sound(samples=samp)
@@ -291,13 +359,17 @@ def make_sine_wave(freq, amp, samp):
     for i in range(samp):
         curr = new_snd.get_sample(i)
         raw_val = math.sin((i / samples_per_period) * max_cycle)
-        curr.set_value(int(raw_val * amp))
+        val = int(raw_val * amp)
+        if DEFAULT_CHANNELS == 1:
+            curr.set_value(val)
+        else:
+            curr.set_values(val, val)
     
     return new_snd
 
 
-def make_square_wave(freq, amp, samp):
-    '''Return a synthesized sound samp samples long in the form of a square wave 
+def create_square_wave(freq, amp, samp):
+    '''Return a Sound samp samples long in the form of a square wave 
     with frequency freq in Hz and amplitude amp in the range [0, 32767].'''
 
     new_snd = Sound(samples=samp)
@@ -314,14 +386,17 @@ def make_square_wave(freq, amp, samp):
             s = 0
         
         curr = new_snd.get_sample(i)
-        curr.set_value(int(sample_val))
+        if DEFAULT_CHANNELS == 1:
+            curr.set_value(int(sample_val))
+        else:
+            curr.set_values(int(sample_val), int(sample_val))
         s += 1
 
     return new_snd
 
 
-def make_triangle_wave(freq, amp, samp):
-    '''Return a synthesized sound samp samples long in the form of a square wave 
+def create_triangle_wave(freq, amp, samp):
+    '''Return a Sound samp samples long in the form of a square wave 
     with frequency freq in Hz and amplitude amp in the range [0, 32767].'''
 
     new_snd = Sound(samples=samp)
@@ -347,17 +422,20 @@ def make_triangle_wave(freq, amp, samp):
         
         curr = new_snd.get_sample(i)
         sample_val = sample_val + increment
-        curr.set_value(int(sample_val))
+        if DEFAULT_CHANNELS == 1:
+            curr.set_value(int(sample_val))
+        else:
+            curr.set_values(int(sample_val), int(sample_val))
         
         quarter_period += 1
         half_period += 1
 
     return new_snd
 
-C = make_sine_wave(264, 6000, 11025)
-D = make_sine_wave(297, 6000, 11025)
-E = make_sine_wave(330, 6000, 11025)
-F = make_sine_wave(352, 6000, 11025)
-G = make_sine_wave(396, 6000, 11025)
-A = make_sine_wave(440, 6000, 11025)
-B = make_sine_wave(494, 6000, 11025)
+C = create_sine_wave(264, 6000, 11025)
+D = create_sine_wave(297, 6000, 11025)
+E = create_sine_wave(330, 6000, 11025)
+F = create_sine_wave(352, 6000, 11025)
+G = create_sine_wave(396, 6000, 11025)
+A = create_sine_wave(440, 6000, 11025)
+B = create_sine_wave(494, 6000, 11025)
