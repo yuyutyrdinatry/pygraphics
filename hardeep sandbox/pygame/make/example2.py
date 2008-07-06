8b415be3c8f25837e31c5300eef5f7a55373fbd3
ootz0rz <ootz0rz@ccdc15fc-0b2f-0410-8e7f-618692008b0f>
2008-07-06 23:56:17 +0000

    tweak to ex2 to properly center on click

--- hardeep sandbox/pygame/make/example2.py
+++ hardeep sandbox/pygame/make/example2.py
@@ -6,7 +6,13 @@ app = hMain('Test')
 app.start()
 
 def event_mouse_left_click(obj, e):
-    obj.set_pos(e.pos)
+    c = e.pos
+    w = obj.look.w
+    h = obj.look.h
+    x = c[0] - (w / 2)
+    y = c[1] - (h / 2)
+    
+    obj.set_pos((x,y))
     obj.look.set_color((randint(0, 255), randint(0, 255), randint(0, 255)))
 
 obj = hObj('MoveRect')
