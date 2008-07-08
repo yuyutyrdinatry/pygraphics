3402f35722622f9074403b76171bd30ecc9b84f8
ootz0rz <ootz0rz@ccdc15fc-0b2f-0410-8e7f-618692008b0f>
2008-07-08 08:55:51 +0000

    made some variable names better (i.e. more readable)

--- trunk/hardeep sandbox/pygame/make/hsdl.py
+++ trunk/hardeep sandbox/pygame/make/hsdl.py
@@ -1,3 +1,4 @@
+# http://wiki.wxpython.org/IntegratingPyGame
 import os, sys
 import wx
 import threading
@@ -21,10 +22,8 @@ class SDLThread(threading.Thread):
         while self.run_thread:
             self._main_loop()
 
-    def stop(self, e=None):
+    def stop(self):
         self.run_thread = False
-        if ( e is not None ):
-            e.Skip()
         
     def _main_loop(self):
         e = pygame.event.poll()
@@ -42,11 +41,11 @@ class SDLThread(threading.Thread):
         pygame.display.flip()
 
 class SDLPanel(wx.Panel):
-    def __init__(self,parent,ID,tplSize):
+    def __init__(self, parent, ID, window_size):
         '''Init pygame display into the Frame as a Panel.'''
         global pygame
         
-        wx.Panel.__init__(self, parent, ID, size=tplSize)
+        wx.Panel.__init__(self, parent, ID, size=window_size)
         self.Fit()
         
         os.environ['SDL_WINDOWID'] = str(self.GetHandle())
@@ -56,7 +55,7 @@ class SDLPanel(wx.Panel):
         import pygame
         
         pygame.display.init()
-        self.window = pygame.display.set_mode(tplSize)
+        self.window = pygame.display.set_mode(window_size)
         
         self.parent = parent
         parent.thread = SDLThread(self.window)
@@ -68,7 +67,8 @@ class SDLPanel(wx.Panel):
         self.parent.stop()
 
 class SDLFrame(wx.Frame):
-    def __init__(self, parent, ID, strTitle, tplSize):
-        s = wx.SYSTEM_MENU|wx.CLOSE_BOX|wx.CAPTION|wx.MINIMIZE_BOX
-        wx.Frame.__init__(self, parent, ID, strTitle, size=tplSize, style=s)
-        self.panel = SDLPanel(self, -1, tplSize)
+    def __init__(self, parent, ID, window_title, window_size):
+        style = wx.SYSTEM_MENU|wx.CLOSE_BOX|wx.CAPTION|wx.MINIMIZE_BOX
+        wx.Frame.__init__(self, parent, ID, window_title, size=window_size, 
+                          style=style)
+        self.panel = SDLPanel(self, -1, window_size)
