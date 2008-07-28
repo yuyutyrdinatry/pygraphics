4186ccd1c18ea4a509e295ddda33336c29999f75
ootz0rz <ootz0rz@ccdc15fc-0b2f-0410-8e7f-618692008b0f>
2008-07-28 23:16:20 +0000

    trying to get it to properly install python modules

--- hardeep sandbox/installer2/RES/_nsis_header.nsi
+++ hardeep sandbox/installer2/RES/_nsis_header.nsi
@@ -10,7 +10,7 @@
     !insertmacro MUI_PAGE_COMPONENTS
     !insertmacro MUI_PAGE_DIRECTORY
     !insertmacro MUI_PAGE_INSTFILES
-    !insertmacro MUI_PAGE_FINISH
+    ;!insertmacro MUI_PAGE_FINISH
     
     ; UNINSTALL
     !insertmacro MUI_UNPAGE_WELCOME
--- hardeep sandbox/installer2/config.py
+++ hardeep sandbox/installer2/config.py
@@ -17,6 +17,8 @@ INST_FILE = 'uoft_installer' # Will automatically append .exe/.pkg/etc...
 # Windows Settings
 #===============================================================================
 WIN_PATH_NSIS = os.path.join('c:\\', 'Program Files', 'NSIS')
+#http://nsis.sourceforge.net/Special_Builds
+WIN_PATH_NSIS = os.path.join('c:\\', 'Program Files', 'nsis-2.38-log')
 WIN_PATH_DEFAULT_INSTALL = "$PROGRAMFILES\uoft"
 
 #===============================================================================
