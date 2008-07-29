2bcd8bf0205ac167f9b6309bf7d3e756f3ed28bd
C.J.Maddison <C.J.Maddison@ccdc15fc-0b2f-0410-8e7f-618692008b0f>
2008-07-29 20:17:49 +0000

    Updated setup.py to have same information as the one that Paul created.

--- trunk/setup.py
+++ trunk/setup.py
@@ -1,13 +1,40 @@
-import os
-from distutils.core import setup
+'''distutils setup for PyGraphics.
+'''
 
-#===============================================================================
-# Core Setup Script
-#===============================================================================
+__docformat__ = 'restructuredtext'
+__version__ = '$Id: setup.py 1855 2008-03-01 11:29:25Z Paul Gries $'
+
+VERSION='2.0'
+
+long_description='''PyGraphics provides a set of easy-to-use procedural media
+manipulation tools and is intended for new programmers.  It is based on a
+Jython library developed by Mark Guzdial at Georgia Tech.'''
+
+from distutils.core import setup
 setup(name='PyGraphics',
-      version='1.3.3.7',
-      extra_path = 'PyGraphics',
-      description='Python Graphics Library',
+      version=VERSION,
+      author='Jen Campbell, Paul Gries, Leo Kaliazine, Chris Maddison, Hardeep Singh',
+      license='GPL',
+      url='http://code.google.com/p/pygraphics',
+      author_email='pgries@cs.toronto.edu',
+      description='Easily-accessible library for media manipulation',
+      long_description=long_description,
+      extra_path = "pygraphics",
+      classifiers=[
+        #'Development Status :: 5 - Production/Stable',
+        'Environment :: MacOS X',
+        'Environment :: Win32 (MS Windows)',
+        'Environment :: X11 Applications',
+        'Intended Audience :: New programmers',
+        #'License :: OSI Approved :: BSD License',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows', # XP
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python',
+        #'Topic :: Games/Entertainment',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+      ],
+
       packages = ['', 'UnitTests'],
       package_dir = {'': 'cpython'},
       )
