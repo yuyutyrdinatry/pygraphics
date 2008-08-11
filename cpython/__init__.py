5f61056145f264e43a62da4e3ebb22951bf7a742
C.J.Maddison <C.J.Maddison@ccdc15fc-0b2f-0410-8e7f-618692008b0f>
2008-08-11 21:59:49 +0000

    The __all__ initialization was broken, and useless. created a small package placeholder.

--- cpython/__init__.py
+++ cpython/__init__.py
@@ -1,19 +1,11 @@
-import os
+'''PyGraphics provides a set of easy-to-use procedural media manipulation tools
+and is intended for new programmers.  It is based on a Jython library 
+developed by Mark Guzdial at Georgia Tech, and has been further developed
+into a Cpython library by Jen Campbell, Paul Gries, Leo Kaliazine, 
+Chris Maddison, and Hardeep Singh.
 
-# IGNORE_FILES is a list of strings, which describe the file names to be 
-# ignored.
-#    i.e. file.name
-IGNORE_FILES = ['__init__.py', 'mediawindows.py', 'setup.py']
+To get started type:
 
-__all__ = []
-for root, dirs, files in os.walk(os.getcwd()):
-    for file in IGNORE_FILES:
-        if ( file in files ):
-            files.remove(file)
-            
-    for file in files:
-        if ( file[-2:] == 'py' and root == os.getcwd()):
-            __all__.append(file)
-            
-if __name__ == '__main__':
-    print __all__
+from media import * 
+
+in the python shell'''
