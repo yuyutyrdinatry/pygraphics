042a191fc2f5d4e3a429213cfecc82f37869d189
pgries <pgries@ccdc15fc-0b2f-0410-8e7f-618692008b0f>
2011-06-21 18:25:18 +0000

    hacky unit tests for media module

---
+++ branches/branch-csc494-summer-2011/cpython/UnitTests/Test_Media.py
@@ -0,0 +1,121 @@
+"""
+Test_Media.py
+"""
+
+import unittest
+import math
+import media
+import color
+
+# Constants for basic width and height.
+WIDTH=10
+HEIGHT=20
+
+
+class MediaCreationTests(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_create_pic_default_color(self):
+        '''Create a WIDTH by HEIGHT picture and check for proper dimensions
+        and default color.'''
+        pic = media.create_picture(WIDTH, HEIGHT)
+        width = media.get_width(pic)
+        height = media.get_height(pic)
+
+        self.assert_(width == WIDTH,
+            'expected pic width of %s, saw %s' % (WIDTH, width))
+
+        self.assert_(height == HEIGHT,
+        'expected pic width of %s, saw %s' % (HEIGHT, height))
+
+        for p in pic:
+            self.assert_(media.get_color(p) == media.white)
+
+    def test_create_pic_color(self):
+        '''Create a WIDTH by HEIGHT picture and check for proper
+        dimensions and color.'''
+        pic = media.create_picture(WIDTH, HEIGHT, media.magenta)
+        width = media.get_width(pic)
+        height = media.get_height(pic)
+
+        self.assert_(width == WIDTH,
+            'expected pic width of %s, saw %s' % (WIDTH, width))
+
+        self.assert_(height == HEIGHT,
+        'expected pic width of %s, saw %s' % (HEIGHT, height))
+
+        for p in pic:
+            self.assert_(media.get_color(p) == media.magenta)
+
+    # Pixel tests.
+
+    def test_get_pixel_top_left(self):
+        """Test getting the top left pixel."""
+
+        pic = media.create_picture(WIDTH, HEIGHT)
+        p = media.get_pixel(pic, 0, 0)
+        self.assert_(media.get_x(p) == 0)
+        self.assert_(media.get_y(p) == 0)
+
+    def test_get_pixel_bottom_right(self):
+        """Test getting the bottom left pixel."""
+
+        pic = media.create_picture(WIDTH, HEIGHT)
+        p = media.get_pixel(pic, WIDTH - 1, HEIGHT - 1)
+        self.assert_(media.get_x(p) == WIDTH - 1)
+        self.assert_(media.get_y(p) == HEIGHT - 1)
+
+    def test_pixel_set_get_RGB(self):
+        """Test setting and getting the red, green, and blue of a pixel."""
+
+        pic = media.create_picture(WIDTH, HEIGHT)
+        p = media.get_pixel(pic, WIDTH - 1, HEIGHT - 1)
+        media.set_red(p, 1)
+        self.assert_(media.get_red(p) == 1)
+        media.set_green(p, 2)
+        self.assert_(media.get_green(p) == 2)
+        media.set_blue(p, 3)
+        self.assert_(media.get_blue(p) == 3)
+
+    def test_pixel_set_get_color(self):
+        """Test setting and getting the color of a pixel."""
+
+        pic = media.create_picture(WIDTH, HEIGHT)
+        p = media.get_pixel(pic, WIDTH - 1, HEIGHT - 1)
+        c = media.aqua
+        media.set_color(p, c)
+        self.assert_(media.get_color(p) == media.aqua)
+
+    def test_darken(self):
+        """Test media.darken."""
+
+        c = color.Color(100, 100, 100)
+        media.darken(c)
+        self.assert_(c.get_red() == 65)
+        self.assert_(c.get_green() == 65)
+        self.assert_(c.get_blue() == 65)
+
+    def test_lighten(self):
+        """Test media.lighten.  These depend on darken.  Boo."""
+
+        c = color.Color(100, 100, 100)
+        media.darken(c)
+        media.lighten(c)
+        self.assert_(c.get_red() == 100)
+        self.assert_(c.get_green() == 100)
+        self.assert_(c.get_blue() == 100)
+
+    # Color tests.
+    
+    def test_distance(self):
+        '''Test the distance function.'''
+        
+        d = media.distance(media.black, media.white)
+        expected = math.sqrt(pow(255, 2) * 3)
+        self.assert_(d == expected, "%s %s" % (d, expected))
+
+
+
+if __name__ == '__main__':
+    unittest.main()
