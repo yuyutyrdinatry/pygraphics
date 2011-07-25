c1204fc5ea3ff65f8607539f4de926cb6bc4bb1c
JeanpierreDA <JeanpierreDA@ccdc15fc-0b2f-0410-8e7f-618692008b0f>
2011-07-25 01:40:40 +0000

    error handling -- and more precise, too

--- branches/branch-csc494-summer-2011/cpython/mediawindows/proxy.py
+++ branches/branch-csc494-summer-2011/cpython/mediawindows/proxy.py
@@ -10,7 +10,7 @@ def _choose_with_remote(command):
 
     try:
         path = mw.callRemote(command, initialdir=LAST_OPENED_DIRECTORY)['path']
-    except:
+    except mw.exceptions.DialogCanceledException:
         return None
     else:
         LAST_OPENED_DIRECTORY = os.path.dirname(path)
@@ -30,9 +30,12 @@ choose_folder = partial(
 
 def choose_color():
     '''Prompt user to pick a color. Return a RGB Color object.'''
-
-    colors = mw.callRemote(mw.amp.AskColor, r=255, g=255, b=255)
-    return (colors['r'], colors['g'], colors['b'])
+    try:
+        colors = mw.callRemote(mw.amp.AskColor, r=255, g=255, b=255)
+    except mw.exceptions.DialogCanceledException:
+        return None
+    else:
+        return (colors['r'], colors['g'], colors['b'])
 
 def say(text):
     mw.callRemote(mw.amp.Say, text=text)
