757b1fc607cb1d52de23756640703f8fcc58abbd
JeanpierreDA <JeanpierreDA@ccdc15fc-0b2f-0410-8e7f-618692008b0f>
2011-07-25 03:31:20 +0000

    It's beta!

--- branches/branch-csc494-summer-2011/setup.py
+++ branches/branch-csc494-summer-2011/setup.py
@@ -23,7 +23,7 @@ setup(name='PyGraphics',
       long_description=long_description,
       extra_path = "pygraphics",
       classifiers=[
-        #'Development Status :: 5 - Production/Stable',
+        'Development Status :: 4 - Beta',
         'Environment :: MacOS X',
         'Environment :: Win32 (MS Windows)',
         'Environment :: X11 Applications',
