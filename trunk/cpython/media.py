2c872ee2e0ecd7a38a72db1a55526877b4f91dd4
pgries <pgries@ccdc15fc-0b2f-0410-8e7f-618692008b0f>
2011-08-29 15:23:41 +0000

    got rid of old sound folder

--- trunk/cpython/media.py
+++ trunk/cpython/media.py
@@ -5,18 +5,15 @@ modules fully.
 Pictures currently support the following formats: JPEG, BMP, GIF, TIFF,
 IM, MSP, PNG, PCX, and PPM.
 
-Sounds support only uncompressed WAV files. For best quality use WAV files
-with sampling rates of either 22050 or 44100. The default number of channels,
-sampling rate, encoding, and buffering can be changed in the sound.py file."""
+Sounds support is self-contained in sound_media.py, and must be imported
+separately."""
 
 from picture import *
-from sound import *
 from color import *
 import mediawindows as mw
 
 mw.init_mediawindows()
 picture.init_picture()
-# init_sound()
 
 
 ####################----------------------------------------------------------
@@ -253,198 +250,6 @@ def create_color(r, g, b):
     """Return a Color object with RGB values r, g, and b."""
 
     return Color(r, g, b)
-
-
-####################----------------------------------------------------------
-## Global Sound Functions
-####################----------------------------------------------------------
-
-
-def load_sound(filename):
-    """Return the Sound at file filename. Requires: file is an uncompressed
-    .wav file."""
-
-    return Sound(filename=filename)
-
-
-def create_sound(samp):
-    """Return a silent Sound samp samples long."""
-
-    return Sound(samples=samp)
-
-
-def create_note(note, samp, octave=0):
-    """Return a Sound samp samples long with the frequency according to
-    str note. The following are acceptable arguments for note, starting
-    at middle C:
-
-    'C', 'D', 'E', 'F', 'G', 'A', and 'B'
-
-    To raise or lower an octave specify the argument octave as a
-    positive or negative int. Positive to raise by that many octaves
-    and negative to lower by that many."""
-
-    return Note(note, samp, octave=octave)
-
-
-def get_samples(snd):
-    """Return a list of Samples in Sound snd."""
-
-    return [samp for samp in snd]
-
-
-def get_max_sample(snd):
-    """Return Sound snd's highest sample value. If snd is stereo
-    return the absolute highest for both channels."""
-
-    snd.get_max()
-
-
-def get_min_sample(snd):
-    """Return Sound snd's lowest sample value. If snd is stereo
-    return the absolute lowest for both channels."""
-
-    snd.get_min()
-
-
-def concatenate(snd1, snd2):
-    """Return a new Sound object with Sound snd1 followed by Sound snd2."""
-
-    return snd1 + snd2
-
-
-def append_silence(snd, samp):
-    """Append samp samples of silence onto Sound snd."""
-
-    snd.append_silence(samp)
-
-
-def crop_sound(snd, first, last):
-    """Crop snd Sound so that all Samples before int first and
-    after int last are removed. Cannot crop to a single sample.
-    Negative indices are supported."""
-
-    snd.crop(first, last)
-
-
-def insert(snd1, snd2, i):
-    """Insert Sound snd2 in Sound snd1 at index i."""
-
-    snd1.insert(snd2, i)
-
-
-def play(snd):
-    """Play Sound snd from beginning to end."""
-
-    snd.play()
-
-
-def play_in_range(snd, first, last):
-    """Play Sound snd from index first to last."""
-
-    snd.play(first, last)
-
-
-def stop(snd):
-    """Stop playing Sound snd."""
-
-    snd.stop()
-
-
-def get_sampling_rate(snd):
-    """Return the Sound snd's sampling rate."""
-
-    return snd.get_sampling_rate()
-
-
-def get_sample(snd, i):
-    """Return Sound snd's Sample object at index i."""
-
-    return snd.get_sample(i)
-
-
-def get_waveform(snd, first=0, last=-1, width=1250, height=128,
-        theme='DEFAULT'):
-    """Return a Picture width pixels wide and height pixels high
-    of the waveform graph of Sound snd from index first to last.
-    Use the color scheme denoted by the str theme. Available arguments
-    are 'DEFAULT', 'PRO', 'OCEAN', 'NOTEBOOK', 'HEART'."""
-
-    snd_copy = snd.copy()
-    snd_copy.crop(first, last)
-    return snd_copy.get_waveform_graph(len(snd_copy) / 12500, width, height,
-        theme)
-
-
-def get_spectrogram(snd, width=1024, height=300):
-    """Unimplemented"""
-
-    pass
-
-
-def get_spectrum(snd, width=1024, height=300):
-    """Unimplemented"""
-
-    pass
-
-
-####################----------------------------------------------------------
-## Global Sample Functions
-####################----------------------------------------------------------
-
-def get_index(samp):
-    """Return Sample samp's index."""
-
-    return samp.get_index()
-
-
-def set_value(mono_samp, v):
-    """Set MonoSample mono_samp's value to v."""
-
-    mono_samp.set_value(v)
-
-
-def get_value(mono_samp):
-    """Return MonoSample mono_samp's value."""
-
-    return mono_samp.get_value()
-
-
-def set_values(stereo_samp, left, right):
-    """Set StereoSample stereo_samp's left value to left and
-    right value to right."""
-
-    stereo_samp.set_values(left, right)
-
-
-def get_values(stereo_samp):
-    """Return StereoSample stereo_samp's values in a tuple, (left, right)."""
-
-    return stereo_samp.get_values()
-
-
-def set_left(stereo_samp, v):
-    """Set StereoSample stereo_samp's left value to v."""
-
-    stereo_samp.set_left(v)
-
-
-def get_left(stereo_samp):
-    """Return StereoSample stereo_samp's left value."""
-
-    return stereo_samp.get_left()
-
-
-def set_right(stereo_samp, v):
-    """Set StereoSample stereo_samp's right value to v."""
-
-    stereo_samp.set_right(v)
-
-
-def get_right(stereo_samp):
-    """Return StereoSample stereo_samp's right value."""
-
-    return stereo_samp.get_right()
 
 
 ####################----------------------------------------------------------
